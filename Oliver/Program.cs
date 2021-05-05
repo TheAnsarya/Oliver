95af04ef3f23c09003c274ba29e73bfb8d543560
Andy Hubbard <me@ansarya.com>
2021-05-05 18:12:04 -0400

    serilog write to file

--- Oliver/Program.cs
+++ Oliver/Program.cs
@@ -1,14 +1,30 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace Oliver {
 	public class Program {
 		public static void Main(string[] args) {
-			CreateHostBuilder(args).Build().Run();
+			Log.Logger = new LoggerConfiguration()
+				.MinimumLevel.Information()
+				.WriteTo.Debug()
+				.WriteTo.File("logs.txt", rollingInterval: RollingInterval.Day)
+				.CreateLogger();
+
+			try {
+				Log.Information("Starting Web Host");
+				CreateHostBuilder(args).Build().Run();
+			} catch (Exception ex) {
+				Log.Fatal(ex, "Host terminated unexpectedly");
+			} finally {
+				Log.CloseAndFlush();
+			}
 		}
 
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
 			Host.CreateDefaultBuilder(args)
+				.UseSerilog()
 				.ConfigureWebHostDefaults(webBuilder => {
 					webBuilder.UseStartup<Startup>();
 				});
